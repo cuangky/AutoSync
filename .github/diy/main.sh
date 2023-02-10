dae02d7e8e81d183c616ce11feec681f937afbed
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-11 01:37:55 +0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -15,7 +15,7 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 rm -r *
-curl -s https://api.github.com/repos/kzer00/packages-compile/releases/tags/02%2F10_2023_13%2F26_x86_64 | grep "browser_download_url.*" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P stb/
+curl -s https://api.github.com/repos/kzer00/packages-compile/releases/tags/aarch64_cortex-a53 | grep "browser_download_url.*" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P aarch64_cortex-a53/
 
 exit 0
 
