73c7259855ca6d27010d6bbcce5e22e9dd123e0c
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-10 14:30:22 +0700

    Update repo.sh

--- .github/diy/repo.sh
+++ .github/diy/repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function git_sparse_clone() {
 branch="$1" rurl="$2" localdir="$3" && shift 3
-git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+curl -s $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
 cd $localdir
 git sparse-checkout init --cone
 git sparse-checkout set $@
@@ -15,7 +15,6 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 curl -s https://api.github.com/repos/kzer00/packages-compile/releases/tags/02%2F10_2023_13%2F26_x86_64 | grep "browser_download_url.*.ipk" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-
 
 exit 0
 
