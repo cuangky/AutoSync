78d144fd4bd730b8cae1946d5f5bec88925d8b85
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-10 14:59:57 +0700

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -14,7 +14,7 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-curl -s https://api.github.com/repos/kzer00/packages-compile/releases/tags/02%2F10_2023_13%2F26_x86_64 | grep "browser_download_url.*.ipk" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+curl -s https://api.github.com/repos/kzer00/packages-compile/releases/tags/02%2F10_2023_13%2F26_x86_64 | grep "browser_download_url.*" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 
 exit 0
 
