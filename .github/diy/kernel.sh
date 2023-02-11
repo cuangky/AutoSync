0efe7baadd3ce999b94317e3875dab838b305360
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-12 02:12:19 +0700

    Create kernel.sh

---
+++ .github/diy/kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function git_sparse_clone() {
+branch="$1" rurl="$2" localdir="$3" && shift 3
+curl -s  $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+cd $localdir
+git sparse-checkout init --cone
+git sparse-checkout set $@
+mv -n $@ ../
+cd ..
+rm -rf $localdir
+}
+curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P kernel
+
+
+ 
+exit 0
