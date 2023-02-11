b9c0508eef1f610f1969a06c70f11781f0794faa
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-11 13:32:30 +0700

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -14,14 +14,14 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-#svn export https://github.com/v2rayA/v2raya-openwrt/trunk/v2raya
-#git clone https://github.com/rufengsuixing/luci-app-zerotier
-#svn export https://github.com/v2rayA/v2raya-openwrt/trunk/luci-app-v2raya
-#git clone https://github.com/4IceG/luci-app-3ginfo op3ginfo  && mv -n op3ginfo/{3ginfo,luci-app-3ginfo} ./;rm -rf op3ginfo
-#git clone https://github.com/koshev-msk/modemfeed && mv -n modemfeed/*/*/* ./ && rm -rf modemfeed
-#git clone https://github.com/4IceG/luci-app-sms-tool smstool && mvdir smstool
-#git clone https://github.com/4IceG/luci-app-modemband modemb  && mv -n modemb/* ./;rm -rf modemb
-#git clone https://github.com/derisamedia/luci-theme-alpha
+svn export https://github.com/v2rayA/v2raya-openwrt/trunk/v2raya
+git clone https://github.com/rufengsuixing/luci-app-zerotier
+svn export https://github.com/v2rayA/v2raya-openwrt/trunk/luci-app-v2raya
+git clone https://github.com/4IceG/luci-app-3ginfo op3ginfo  && mv -n op3ginfo/{3ginfo,luci-app-3ginfo} ./;rm -rf op3ginfo
+git clone https://github.com/koshev-msk/modemfeed && mv -n modemfeed/*/*/* ./ && rm -rf modemfeed
+git clone https://github.com/4IceG/luci-app-sms-tool smstool && mvdir smstool
+git clone https://github.com/4IceG/luci-app-modemband modemb  && mv -n modemb/* ./;rm -rf modemb
+git clone https://github.com/derisamedia/luci-theme-alpha
 
 #git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
