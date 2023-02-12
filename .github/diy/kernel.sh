384751ef25fddc9991796793a937ecf97ddf9bd9
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-12 22:02:40 +0700

    Update kernel.sh

--- .github/diy/kernel.sh
+++ .github/diy/kernel.sh
@@ -9,10 +9,10 @@ git sparse-checkout init --cone
 git sparse-checkout set $@
 mv -n $@ ../
 cd ..
-#rm -rf $localdir
+rm -rf $localdir
 }
 
-svn export https://github.com/lynxnexy/openwrt/trunk/include/kernel pub/stable && mv * pub/stable
-#svn export  https://github.com/kzer00/k3rnel/trunk/pub/stable
-#curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  stable && cd stable && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
+svn export https://github.com/lynxnexy/openwrt/trunk/include/kernel pub/stable 
+svn export  https://github.com/kzer00/k3rnel/trunk/pub/stable pub/stable 
+curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  pub/stable  && cd pub/stable  && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
 exit 0
