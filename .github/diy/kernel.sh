79130bfbf73f8906c117e94e0656ba8d4be20e09
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-12 16:30:49 +0700

    Update kernel.sh

--- .github/diy/kernel.sh
+++ .github/diy/kernel.sh
@@ -13,5 +13,5 @@ rm -rf $localdir
 
 
 svn export  https://github.com/kzer00/k3rnel/trunk/pub/stable
-curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  stable && cd pub/stable && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
+curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  stable && cd stable && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
 exit 0
