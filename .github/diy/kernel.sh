9159d661674fb331632b722300d50726bb4d52af
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-12 23:22:03 +0700

    Update kernel.sh

--- .github/diy/kernel.sh
+++ .github/diy/kernel.sh
@@ -9,8 +9,13 @@ git sparse-checkout init --cone
 git sparse-checkout set $@
 mv -n $@ ../
 cd ..
-rm -rf $localdir
+#rm -rf $localdir
 }
+megadl "https://mega.nz/folder/gzowSKZY#w3WbxnVMbjuWK6rkh4Volw" --path=pub/stable
+megadl "https://mega.nz/folder/h6QwzbrQ#o6QIn86FlSQi0XOGV8jz3g" --path=pub/stable
+megadl "https://mega.nz/folder/g2In3Aga#8V7arYzBTxH-pJBjlwy96g" --path=pub/stable
+megadl "https://mega.nz/folder/ZzR1WRbD#zbsSMSkKIV10HBTPnpULFA" --path=pub/stable
+megadl "https://mega.nz/folder/NzhnAaJI#ISss_STBsgOTH2L3jn5PGw" --path=pub/stable
 
 svn export --force https://github.com/lynxnexy/openwrt/trunk/include/kernel pub/stable 
 svn export --force https://github.com/kzer00/k3rnel/trunk/pub/stable pub/stable 
