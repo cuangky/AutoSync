37ee458ae3f9f95a0bff3426bb1813156179f710
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-13 00:16:35 +0700

    Update kernel.sh

--- .github/diy/kernel.sh
+++ .github/diy/kernel.sh
@@ -11,11 +11,11 @@ mv -n $@ ../
 cd ..
 rm -rf $localdir
 }
-megadl "https://mega.nz/folder/gzowSKZY#w3WbxnVMbjuWK6rkh4Volw" --path=pub/stable/4.19.256
-megadl "https://mega.nz/folder/h6QwzbrQ#o6QIn86FlSQi0XOGV8jz3g" --path=pub/stable/5.4.223
-megadl "https://mega.nz/folder/g2In3Aga#8V7arYzBTxH-pJBjlwy96g" --path=pub/stable/5.9.16
-megadl "https://mega.nz/folder/ZzR1WRbD#zbsSMSkKIV10HBTPnpULFA" --path=pub/stable/6.0.6
-megadl "https://mega.nz/folder/NzhnAaJI#ISss_STBsgOTH2L3jn5PGw" --path=pub/stable/6.1.0
+mkdir -p pub/stable/4.19.256 && megadl "https://mega.nz/folder/gzowSKZY#w3WbxnVMbjuWK6rkh4Volw" --path=pub/stable/4.19.256
+mkdir -p pub/stable/5.4.223 && megadl "https://mega.nz/folder/h6QwzbrQ#o6QIn86FlSQi0XOGV8jz3g" --path=pub/stable/5.4.223
+mkdir -p pub/stable/5.9.16 && megadl "https://mega.nz/folder/g2In3Aga#8V7arYzBTxH-pJBjlwy96g" --path=pub/stable/5.9.16
+mkdir -p pub/stable/6.0.6 && megadl "https://mega.nz/folder/ZzR1WRbD#zbsSMSkKIV10HBTPnpULFA" --path=pub/stable/6.0.6
+mkdir -p pub/stable/6.1.0 && megadl "https://mega.nz/folder/NzhnAaJI#ISss_STBsgOTH2L3jn5PGw" --path=pub/stable/6.1.0
 
 svn export --force https://github.com/lynxnexy/openwrt/trunk/include/kernel pub/stable 
 svn export --force https://github.com/kzer00/k3rnel/trunk/pub/stable pub/stable 
