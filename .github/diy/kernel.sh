9931746d4f2a16cfc807ac331d8c86cc46a37561
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-12 21:56:44 +0700

    Update kernel.sh

--- .github/diy/kernel.sh
+++ .github/diy/kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p pub/stable
+
 function git_sparse_clone() {
 branch="$1" rurl="$2" localdir="$3" && shift 3
 curl -s  $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
@@ -12,7 +12,7 @@ cd ..
 #rm -rf $localdir
 }
 
-svn export https://github.com/lynxnexy/openwrt/trunk/include/kernel && mv -n kernel/* pub/stable/* ; rm -rf kernel
+svn export https://github.com/lynxnexy/openwrt/trunk/include/kernel pub/stable
 #svn export  https://github.com/kzer00/k3rnel/trunk/pub/stable
 #curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  stable && cd stable && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
 exit 0
