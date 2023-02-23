23bc5d20afc08bd88d2bb261a5a6ab040352df07
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-24 06:23:28 +0700

    Create iptv.sh

---
+++ .github/diy/iptv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+function git_sparse_clone() {
+branch="$1" rurl="$2" localdir="$3" && shift 3
+curl -s  $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+cd $localdir
+git sparse-checkout init --cone
+git sparse-checkout set $@
+mv -n $@ ../
+cd ..
+#rm -rf $localdir
+}
+
+svn export --force  https://github.com/SSHSEDANG4/iptv
+
+exit 0
