b6707eb26b88ea33a41ac70833344e79302ce87a
cuangky <124901681+cuangky@users.noreply.github.com>
2023-02-24 07:00:49 +0700

    Update iptv.sh

--- .github/diy/iptv.sh
+++ .github/diy/iptv.sh
@@ -3,7 +3,7 @@
 
 function git_sparse_clone() {
 branch="$1" rurl="$2" localdir="$3" && shift 3
-git clone -s  $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+git clone -b  $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
 cd $localdir
 git sparse-checkout init --cone
 git sparse-checkout set $@
@@ -12,6 +12,6 @@ cd ..
 rm -rf $localdir
 }
 
-git clone https://github.com/SSHSEDANG4/iptv
+git clone --depth 1 https://github.com/SSHSEDANG4/iptv
 
 exit 0
