6e08cc6c3940cb49718879eb06fbd0788b30d217
cuangky <124901681+cuangky@users.noreply.github.com>
2023-03-01 23:26:58 +0700

    Update rooter.sh

--- .github/diy/rooter.sh
+++ .github/diy/rooter.sh
@@ -16,9 +16,9 @@ function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
-svn export --force https://github.com/ofmodemsandmen/RooterSource/trunk/package/rooter-builds && mv -n rooter-builds/0protocols rooter-builds/ext-huasifei rooter-builds/ext-huasifel16 rooter-builds/ext-rooter-lite rooter-builds/ext-rooter16 rooter-builds/ext-rooter4 rooter-builds/ext-rooter8-vpn rooter-builds/ext-rooterbcm16 ./ ; rm -rf rooter-builds 
-
-
+svn export --force https://github.com/ofmodemsandmen/RooterSource/trunk/package/rooter-builds && mv -n rooter-builds/0protocols rooter-builds/ext-huasifei rooter-builds/ext-huasifei16 rooter-builds/ext-rooter-lite rooter-builds/ext-rooter16 rooter-builds/ext-rooter4 rooter-builds/ext-rooter8-vpn rooter-builds/ext-rooterbcm16 ./ ; rm -rf rooter-builds 
+svn export --force https://github.com/ofmodemsandmen/RooterSource/trunk/package/rooter && mv -n rooter/0basicapps rooter/0basicsupport rooter/0drivers rooter/0optionalapps rooter/0routerspecfic rooter/0splash rooter/0themes rooter/0wifi rooter/ext-rooter-basic ./ ; rm -rm rooter
+svn export --force https://github.com/ofmodemsandmen/RooterSource/tree/main/package/rooter-extra && mv -n rooter-extra/dawn19 rooter-extra/eoip rooter-extra/ext-simple-gps rooter-extra/luci-app-dawn19 rooter-extra/mesh11sd rooter-extra/speedtestpp ./ ;rm -rf rooter-extra
 
 
 
