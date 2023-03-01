921c86f54580e7540670a23da79f6225c023ff2c
cuangky <124901681+cuangky@users.noreply.github.com>
2023-03-01 23:59:15 +0700

    Update rooter.sh

--- .github/diy/rooter.sh
+++ .github/diy/rooter.sh
@@ -19,7 +19,8 @@ rm -rf $1
 svn export --force https://github.com/ofmodemsandmen/RooterSource/trunk/package/rooter-builds && mv -n rooter-builds/0protocols rooter-builds/ext-huasifei rooter-builds/ext-huasifei16 rooter-builds/ext-rooter-lite rooter-builds/ext-rooter16 rooter-builds/ext-rooter4 rooter-builds/ext-rooter8-vpn rooter-builds/ext-rooterbcm16 ./ ; rm -rf rooter-builds 
 svn export --force https://github.com/ofmodemsandmen/RooterSource/trunk/package/rooter && mv -n rooter/0basicapps rooter/0basicsupport rooter/0drivers rooter/0optionalapps rooter/0routerspecfic rooter/0splash rooter/0themes rooter/0wifi rooter/ext-rooter-basic ./ ; rm -rf rooter
 svn export --force https://github.com/ofmodemsandmen/RooterSource/trunk/package/rooter-extra && mv -n rooter-extra/dawn19 rooter-extra/eoip rooter-extra/ext-simple-gps rooter-extra/luci-app-dawn19 rooter-extra/mesh11sd rooter-extra/speedtestpp ./ ;rm -rf rooter-extra
-
+svn export --force https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/network/utils/umbim
+svn export --force https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/network/utils/uqmi
 
 
 
