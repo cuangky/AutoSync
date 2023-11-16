4783115b50d4d074d13165fea43ae548a78dfd86
cuangky <124901681+cuangky@users.noreply.github.com>
2023-11-17 00:51:14 +0700

    Update kernel.sh

--- .github/diy/kernel.sh
+++ .github/diy/kernel.sh
@@ -11,7 +11,7 @@ mv -n $@ ../
 cd ..
 #rm -rf $localdir
 }
-mkdir -p newkernel && git clone --depth 1 https://github.com/ophub/kernel/releases/download/kernel_stable/5.10.195.tar.gz /newkernel
+#mkdir -p newkernel && git clone --depth 1 https://github.com/ophub/kernel/releases/download/kernel_stable/5.10.195.tar.gz /newkernel
 #mkdir -p pub/stable/4.19.256 && megadl "https://mega.nz/folder/gzowSKZY#w3WbxnVMbjuWK6rkh4Volw" --path=pub/stable/4.19.256
 #mkdir -p pub/stable/5.4.223 && megadl "https://mega.nz/folder/h6QwzbrQ#o6QIn86FlSQi0XOGV8jz3g" --path=pub/stable/5.4.223
 #mkdir -p pub/stable/5.9.16 && megadl "https://mega.nz/folder/g2In3Aga#8V7arYzBTxH-pJBjlwy96g" --path=pub/stable/5.9.16
@@ -20,5 +20,5 @@ mkdir -p newkernel && git clone --depth 1 https://github.com/ophub/kernel/releas
 #mkdir -p pub/stable/6.2.0 && megadl "https://mega.nz/folder/ZzIXBbJI#QmpXHOtQ_Jox0CJWWtBzkA" --path=pub/stable/6.2.0
 #svn export --force https://github.com/lynxnexy/openwrt/trunk/include/kernel pub/stable 
 #svn export --force https://github.com/kzer00/k3rnel/trunk/pub/stable pub/stable 
-#curl -s https://api.github.com/repos/kzer00/amlogic-s9xxx-openwrt/releases/latest | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  pub/stable  && cd pub/stable  && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
+curl -s https://api.github.com/repos/ophub/kernel/releases/kernel_stable | grep "browser_download_url.*.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi - -P  pub/stable  && cd pub/stable  && ls *tar.gz | xargs -n 1 tar -zxvf && rm -r *tar.gz
 exit 0
